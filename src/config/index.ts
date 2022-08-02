75d380daf177950e350864a6120854804e092ab8
徐世成 <randaoshengxia@gmail.com>
2022-08-02 20:34:15 +0800

    add: 秋炉认证用服务器

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,14 @@
-export const AXIOSURL = "http://121.37.25.71:10004"
-export const AXIOSTIMEOUT = 60000
-export const ADMINURL = "http://121.37.25.71:10002"
-export const IMURL = "ws://121.37.25.71:10003"
-export const PICMESSAGETHUMOPTION = "?imageView2/1/w/200/h/200/rq/80"
-export const LANGUAGE = "zh-cn"
+// 秋炉认证用服务器
+export const QCOLEURL = "http://gengxin.qcole.com";
 
-export const getIMUrl = () => localStorage.getItem("IMUrl")?localStorage.getItem("IMUrl")!:IMURL
-export const getAxiosUrl = () => localStorage.getItem("IMAxiosUrl")?localStorage.getItem("IMAxiosUrl")!:AXIOSURL
-export const getAdminUrl = () => localStorage.getItem("IMAdminUrl")?localStorage.getItem("IMAdminUrl")!:ADMINURL
-export const getLanguage = () => localStorage.getItem("IMLanguage")?localStorage.getItem("IMLanguage")!:LANGUAGE
+export const AXIOSURL = "http://im.sevencodes.cn:10004";
+export const AXIOSTIMEOUT = 60000;
+export const ADMINURL = "http://im.sevencodes.cn:10002";
+export const IMURL = "ws://im.sevencodes.cn:10003";
+export const PICMESSAGETHUMOPTION = "?imageView2/1/w/200/h/200/rq/80";
+export const LANGUAGE = "zh-cn";
+
+export const getIMUrl = () => (localStorage.getItem("IMUrl") ? localStorage.getItem("IMUrl")! : IMURL);
+export const getAxiosUrl = () => (localStorage.getItem("IMAxiosUrl") ? localStorage.getItem("IMAxiosUrl")! : AXIOSURL);
+export const getAdminUrl = () => (localStorage.getItem("IMAdminUrl") ? localStorage.getItem("IMAdminUrl")! : ADMINURL);
+export const getLanguage = () => (localStorage.getItem("IMLanguage") ? localStorage.getItem("IMLanguage")! : LANGUAGE);
