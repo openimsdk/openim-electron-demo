0c075165e542c35943e02c3d6f1b6d6bc6c6f884
kevin <1026838160@qq.com>
2024-08-09 17:40:42 +0800

    feat: base update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 // WS 10001 API 10002 CHAT 10008 CONFIG 10009
 
-// export const WS_URL = "wss://web.rentsoft.cn/msg_gateway";
-// export const API_URL = "https://web.rentsoft.cn/api";
-// export const USER_URL = "https://web.rentsoft.cn/chat";
+// export const WS_URL = "wss://xxx.com/msg_gateway";
+// export const API_URL = "https://xxx.com/api";
+// export const USER_URL = "https://xxx.com/chat";
 
 export const WS_URL = "ws://your-server-ip:10001";
 export const API_URL = "http://your-server-ip:10002";
